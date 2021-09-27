5b0f94d302573359a864dc5c8e13e741169a21f7
ysh <cysh1346@gmail.com>
2021-09-27 21:18:48 +0900

    완성!

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,6 @@
 package racingcar;
 
 import inputview.InputView;
-import utils.RandomUtils;
 import utils.Splitor;
 
 import java.util.List;
@@ -9,7 +8,6 @@ import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
         String carNames = InputView.inputCarNames();
         int tryCount = InputView.inputTryCount();
 
--- src/main/java/racingcar/CarGame.java
+++ src/main/java/racingcar/CarGame.java
@@ -1,12 +1,13 @@
 package racingcar;
 
+import utils.GameResult;
 import utils.RandomUtils;
 
 import java.util.List;
 
 public class CarGame {
-    private int tryCount;
-    private List<Car> cars;
+    private final int tryCount;
+    private final List<Car> cars;
 
     public CarGame(int tryCount, List<Car> cars) {
         this.tryCount = tryCount;
@@ -17,10 +18,21 @@ public class CarGame {
         for (int i = 0; i < tryCount; i++) {
             loop();
         }
+
+        printResult();
     }
 
     private void loop() {
         cars.forEach(car -> car.move(RandomUtils.nextInt(0, 9)));
-        System.out.println(cars.toString());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Car car : cars) {
+            stringBuilder.append(car.toString()).append("\n");
+        }
+        System.out.println(stringBuilder);
+    }
+
+    private void printResult() {
+        GameResult gameResult = new GameResult(cars);
+        System.out.println("result: " + gameResult.toStringWinner());
     }
 }
